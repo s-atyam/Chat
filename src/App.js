0922ba0aa020e567726702fd09715f1501628918
SATYAM <satyambharti080@gmail.com>
2022-06-22 01:59:19 +0530

    version 0.3 (base)

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ChatRoom from './components/chatRoom';
+import JoinRoom from './components/joinRoom';
 import CreateRoom from './components/createRoom';
 import ChatData from './context/ChatData';
+
+import io from 'socket.io-client';
+
+const socket = io.connect('http://localhost:5000');
 
 
 function App() {
@@ -11,10 +16,12 @@ function App() {
   return (
     <>
     <ChatData>
+      <div>{socket.id}</div>
       <Router>
         <Routes>
-          <Route exact path="/" element={<CreateRoom />} />
-          <Route exact path="/chat" element={<ChatRoom />} />
+          <Route exact path="/" element={<JoinRoom socket={socket} />} />
+          <Route exact path="/create" element={<CreateRoom socket={socket} />} />
+          <Route exact path="/chat" element={<ChatRoom socket={socket}/>} />
         </Routes>
       </Router>
       </ChatData>
--- src/components/chatRoom.js
+++ src/components/chatRoom.js
@@ -1,22 +1,19 @@
 import { useEffect,useContext } from 'react';
 import chatContext from '../context/chatContext';
-import io from 'socket.io-client';
 
-const socket = io.connect('http://localhost:5000');
-
-const chatRoom = () => {
+const chatRoom = (props) => {
 
   const stateVariable = useContext(chatContext);
 
   const sendChat = (e) => {
     e.preventDefault();
-    socket.emit('chat', { message:stateVariable.message , "iddiv": socket.id });
+    props.socket.emit('chat', { message:stateVariable.message , "iddiv": props.socket.id });
     stateVariable.setMessage('');
 
   }
 
   useEffect(() => {
-    socket.on('chat', (payload) => {
+    props.socket.on('chat', (payload) => {
       stateVariable.setChat([...stateVariable.chat, payload]);
     });
   });
@@ -27,12 +24,12 @@ const chatRoom = () => {
       <div onChange={setTimeout(()=>{document.getElementById('mainArea').scrollTop = document.getElementById('mainArea').scrollHeight},0)} id='mainArea' className='bg-slate-200 rounded-md w-1/2 min-w-min h-3/5 px-2 py-2 overflow-auto'>
         {stateVariable.chat.map((payload, index) => {
           return (
-            <p className={`bg-slate-300 rounded-md h-8 w-fit ${socket.id===payload.iddiv?'ml-auto':'mr-auto'} text-lg text-slate-600 px-4 mb-1`} key={index}>{payload.message}</p>
+            <p className={`bg-slate-300 rounded-md h-8 w-fit ${props.socket.id===payload.iddiv?'ml-auto':'mr-auto'} text-lg text-slate-600 px-4 mb-1`} key={index}>{payload.message}</p>
           )
         })}
       </div>
       <div className='flex items-center justify-center w-1/2 min-w-min mt-2'>
-        <input className='bg-slate-100 rounded-md h-12 w-full text-lg text-slate-500 mr-2 pl-4' style={{ "outline": "none", "border": "none" }} type='text' name='chat' placeholder='Type your text here' value={stateVariable.message} onChange={(e)=>{stateVariable.setMessage(e.target.value)}}/>
+        <input className='bg-slate-100 rounded-md h-12 w-full text-lg text-slate-500 mr-2 pl-4' style={{ "outline": "none", "border": "none" }} type='text' name='chat' placeholder={props.socket.id} value={stateVariable.message} onChange={(e)=>{stateVariable.setMessage(e.target.value)}}/>
         <button className='bg-slate-200 text-slate-500 rounded-md w-40 h-12 text-xl border hover:text-slate-700' style={{ "border": "none" }} type='submit' onClick={sendChat}>Send</button>
       </div>
     </div>
